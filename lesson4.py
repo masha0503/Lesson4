0a8b1a73b684a22ebd256be77ec8a06be5ed19ee
Mariia <mariiaruskikh@gmail.com>
2024-05-15 20:48:42 +0300

    lesson 5

--- lesson4.py
+++ lesson4.py
@@ -65,14 +65,42 @@ class Group:
     def info_group(self):
         print(f'Group: {self.name} | {self.age_category} | {self.num_students} students')
 
-# Создаем экземпляр класса Group
+# Створюємо екземпляр класа Group
 group = Group(name='С2126', num_students=17, age_category='13-15')
 group.info_group()
 
-# Информация о студенте
+# Інформація про студента
 student = Student(name='Mariia', surname='Ruskikh', age=14, progress='good', group=group)
 student.info_all()
 
-# Информация о работнике
+# Інформауія про працівника
 worker = Worker(name='Mariia', surname='Ruskikh', age=14, position='art designer', duties='advertising brochure design')
 worker.info_all()
+
+# Список атрибутів для кожного класу
+person_attributes = [attr for attr in dir(Person) if not callable(getattr(Person, attr)) and not attr.startswith("__")]
+teacher_attributes = [attr for attr in dir(Teacher) if not callable(getattr(Teacher, attr)) and not attr.startswith("__")]
+student_attributes = [attr for attr in dir(Student) if not callable(getattr(Student, attr)) and not attr.startswith("__")]
+worker_attributes = [attr for attr in dir(Worker) if not callable(getattr(Worker, attr)) and not attr.startswith("__")]
+group_attributes = [attr for attr in dir(Group) if not callable(getattr(Group, attr)) and not attr.startswith("__")]
+
+# Вывод списка атрибутів на консоль
+print("Атрибуты класса Person:", person_attributes)
+print("\nАтрибуты класса Teacher:", teacher_attributes)
+print("\nАтрибуты класса Student:", student_attributes)
+print("\nАтрибуты класса Worker:", worker_attributes)
+print("\nАтрибуты класса Group:", group_attributes)
+
+# Список методів для каждого класу
+person_methods = [method for method in dir(Person) if callable(getattr(Person, method))]
+teacher_methods = [method for method in dir(Teacher) if callable(getattr(Teacher, method))]
+student_methods = [method for method in dir(Student) if callable(getattr(Student, method))]
+worker_methods = [method for method in dir(Worker) if callable(getattr(Worker, method))]
+group_methods = [method for method in dir(Group) if callable(getattr(Group, method))]
+
+# Вивод списка методів на консоль
+print("Методы класса Person:", person_methods)
+print("\nМетоды класса Teacher:", teacher_methods)
+print("\nМетоды класса Student:", student_methods)
+print("\nМетоды класса Worker:", worker_methods)
+print("\nМетоды класса Group:", group_methods)
